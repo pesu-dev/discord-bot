9b43254cae5c0751d443a15f68bfdbf4cebdb424
dotpmm <pmmdot@gmail.com>
2025-08-01 17:39:29 +0530

    Update bot.py
    Initialized the db calls to `none` at the start and made them optional with async. This was to make sure the bot doesn't pull up errors when testing without a DB.

--- src/bot.py
+++ src/bot.py
@@ -3,16 +3,17 @@ from discord.ext import commands
 from pymongo.asynchronous.database import AsyncDatabase
 from pymongo.asynchronous.collection import AsyncCollection
 from pymongo import AsyncMongoClient
+from typing import Optional
 
 
 class DiscordBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.mongo_client: AsyncMongoClient
-        self.db: AsyncDatabase
-        self.link_collection: AsyncCollection
-        self.student_collection: AsyncCollection
-        self.anonban_collection: AsyncCollection
-        self.mute_collection: AsyncCollection
+        self.mongo_client: Optional[AsyncMongoClient] = None
+        self.db: Optional[AsyncDatabase] = None
+        self.link_collection: Optional[AsyncCollection] = None
+        self.student_collection: Optional[AsyncCollection] = None
+        self.anonban_collection: Optional[AsyncCollection] = None
+        self.mute_collection: Optional[AsyncCollection] = None
         self.startTime: float
         self.logger = logging.getLogger("discord.app")
